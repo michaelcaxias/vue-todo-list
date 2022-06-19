4f14699cc16a9644a7ddee5efdb6d2f9f937eeac
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-06-19 12:56:45 -0300

    :sparkles: feat: adiciona primeiro hello world!

--- src/components/HelloWorld/HelloWorld.vue
+++ src/components/HelloWorld/HelloWorld.vue
@@ -4,6 +4,10 @@ defineProps<{ msg: string }>();
 
 <template>
   <div class="page">
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/512px-Vue.js_Logo_2.svg.png"
+      alt="vue logo"
+    />
     <h1 class="hello-world">{{ msg }}</h1>
   </div>
 </template>
