c29167ad80e530ad7109442cee87c9491418c176
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-06-19 14:27:58 -0300

    :sparkles: feat: adiciona método para adicionar itens em uma lista

--- src/components/TodoList/TodoList.vue
+++ src/components/TodoList/TodoList.vue
@@ -1,14 +1,34 @@
-<script setup lang="ts">
-defineProps<{ msg: string }>();
+<script lang="ts">
+export default {
+  data() {
+    return {
+      parentMessage: 'Parent',
+      todos: ['oi'],
+    };
+  },
+  methods: {
+    addTodo(event) {
+      if (event.target.value) {
+        this.todos.push(event.target.value);
+        event.target.value = '';
+      }
+    },
+  },
+};
 </script>
 
 <template>
   <div class="container">
-    <img
-      src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/512px-Vue.js_Logo_2.svg.png"
-      alt="vue logo"
-    />
-    <h1 class="hello-world">{{ msg }}</h1>
+    <h1>Todo List</h1>
+    <div>
+      <input type="text" @change="addTodo" />
+      <button type="button" @click="addTodo">Adicionar</button>
+      <ul>
+        <li v-for="todo in todos" :key="todo">
+          {{ todo }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
