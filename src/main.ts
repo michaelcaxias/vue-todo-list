01e8550ac4a46a281d478e11411b7af2cdc9fbff
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-06-19 16:22:52 -0300

    :package: build: instala feather-icons

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VueFeather from 'vue-feather';
+
 import App from './App.vue'
 
 const app = createApp(App)
 
+app.component(VueFeather.name, VueFeather);
 app.use(createPinia())
 
 app.mount('#app')
